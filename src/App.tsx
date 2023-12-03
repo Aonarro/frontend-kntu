964b4bdb4631b1b5be260fe27fabcf8267891071
Nikolay <nikolay.tyshchenko04@gmail.com>
2023-12-03 14:57:54 +0200

    Add TypeScript support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function Square({ value, onSquareClick }) {
+interface SquareProps {
+  value: string | null;
+  onSquareClick: () => void;
+}
+
+function Square({ value, onSquareClick }: SquareProps) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -9,8 +14,14 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-function Board({ xIsNext, squares, onPlay }) {
-  function handleClick(i) {
+interface BoardProps {
+  xIsNext: boolean;
+  squares: Array<string | null>;
+  onPlay: (nextSquares: Array<string | null>) => void;
+}
+
+function Board({ xIsNext, squares, onPlay }: BoardProps) {
+  function handleClick(i: number) {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -28,9 +39,9 @@ function Board({ xIsNext, squares, onPlay }) {
   }
 
   // Генерація рядків та квадратів за допомогою двох циклів
-  const rows = [];
+  const rows: JSX.Element[] = [];
   for (let i = 0; i < 3; i++) {
-    const rowSquares = [];
+    const rowSquares: JSX.Element[] = [];
     for (let j = 0; j < 3; j++) {
       const index = i * 3 + j;
       rowSquares.push(
@@ -56,19 +67,23 @@ function Board({ xIsNext, squares, onPlay }) {
   );
 }
 
-export default function Game() {
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
+interface GameProps {}
+
+export default function Game({}: GameProps) {
+  const [history, setHistory] = useState<Array<Array<string | null>>>([
+    Array(9).fill(null),
+  ]);
+  const [currentMove, setCurrentMove] = useState<number>(0);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
+  function handlePlay(nextSquares: Array<string | null>) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
   }
 
-  function jumpTo(nextMove) {
+  function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
   }
 
@@ -98,7 +113,7 @@ export default function Game() {
   );
 }
 
-function calculateWinner(squares) {
+function calculateWinner(squares: Array<string | null>): string | null {
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -112,7 +127,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return squares[a] as string;
     }
   }
   return null;
