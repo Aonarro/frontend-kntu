150b0fb326b8855a096b4e9037251f007b9ee6be
Nikolay <nikolay.tyshchenko04@gmail.com>
2023-12-03 14:21:56 +0200

    lb-1 react calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [operation, setOperation] = useState('+');
+  const [result, setResult] = useState('');
+
+  const calculateResult = () => {
+    const num1 = parseFloat(input1);
+    const num2 = parseFloat(input2);
+
+    switch (operation) {
+      case '+':
+        setResult(num1 + num2);
+        break;
+      case '-':
+        setResult(num1 - num2);
+        break;
+      case '*':
+        setResult(num1 * num2);
+        break;
+      case '/':
+        setResult(num1 / num2);
+        break;
+      default:
+        setResult('Invalid operation');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Calculator</h1>
+      <input
+        type="number"
+        value={input1}
+        onChange={(e) => setInput1(e.target.value)}
+        placeholder="Enter number 1"
+      />
+      <select value={operation} onChange={(e) => setOperation(e.target.value)}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input
+        type="number"
+        value={input2}
+        onChange={(e) => setInput2(e.target.value)}
+        placeholder="Enter number 2"
+      />
+      <button onClick={calculateResult}>=</button>
+      <input type="text" value={result} readOnly />
     </div>
   );
 }
